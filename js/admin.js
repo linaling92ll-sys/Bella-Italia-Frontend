c8c3a94147e414626dc4bce8c0d9bd2d2202357a
linaling92ll-sys <linaling92.ll@gmail.com>
2026-06-03 09:08:59 +0200

    Skrivit kod för att kunna logga ut

--- js/admin.js
+++ js/admin.js
@@ -6,3 +6,15 @@ if (!token) {
     window.location.href ="login.html";
 }
 
+//Hämtar logga ut knappen
+const logoutBTN = document.getElementById("logout-btn");
+
+//Lyssnar efter klick
+logoutBTN.addEventListener("click", () => {
+
+    //Tar bort token
+    localStorage.removeItem("token");
+
+    //Skickar användaren till logga in sidan
+    window.location.href = "login.html";
+});
